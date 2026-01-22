11ab2fb5bef9d1316f14288e7f36fd8de2e102dc
Sjayp <Sjaypeter.sjp@gmail.com>
2026-01-22 12:24:03 +0100

    feat:add models for pet app

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -1,4 +1,3 @@
-# apps/accounts/serializers.py
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
 from .models import User, VetProfile, ClientProfile
