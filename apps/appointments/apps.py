42c400b7e748248396e0df8df87e5bcc2bcdc146
Sjayp <Sjaypeter.sjp@gmail.com>
2026-01-23 21:44:59 +0100

    refactor:apps.py for appointments and pets

--- apps/appointments/apps.py
+++ apps/appointments/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
-
 class AppointmentsConfig(AppConfig):
-    name = 'appointments'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.appointments'
+    verbose_name = 'Appointments'
